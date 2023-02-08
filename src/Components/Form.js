82cc14fe188d9d1b3e5f0d114b7f7a5d9cc020ee
JitenderKumar <jk204164@gmail.com>
2023-02-08 16:52:02 +0530

    added header and input form

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import './Form.css';
+
+function Form(){
+    return(
+        <form>
+            <input type='text' placeholder="Enter a Todo..." />
+            <button>Add</button>
+        </form>
+    )
+}
+
+export default Form
